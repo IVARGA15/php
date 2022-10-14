368e8fcf5915327d6b55aeb6bad9eea1fb962f31
IVARGA15 <113476624+IVARGA15@users.noreply.github.com>
2022-10-14 10:01:24 -0500

    Add files via upload

---
+++ act1.php
@@ -0,0 +1,40 @@
+<?php
+$day = 3;
+
+switch ($day)
+{
+    case '1';
+        "Monday";
+    break;
+
+    case '2';
+        "Tuesday";
+    break;
+
+    case '3';
+        echo " The day is Wednesday";
+    break;
+
+    case '4';
+        "Thursday";
+    break;
+
+    case '5';
+        "Friday";
+    break;
+
+    case '6';
+        "saturday";
+    break;
+
+    case '7';
+        "Sunday";
+    break;
+
+
+    default;
+    "There is not an 8 day";
+}
+
+
+?>
